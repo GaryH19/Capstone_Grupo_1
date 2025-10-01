dd40762523d2ef2ce07134d3714eb67f4142e015
Gary Hernandez <ghernandez@jriverosdomain.jriveros.cl>
2025-10-01 17:22:29 -0300

    Actualizacicon: Login y Formulario de Registro/ Agregado de css

--- DocuFlow/settings.py
+++ DocuFlow/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,6 +106,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'formulario', 'static'), # Esto apunta a Capstone_Grupo_1/formulario/static
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
