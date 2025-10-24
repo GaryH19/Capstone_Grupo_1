7d5c1b80244619469e29df565a5f9d541ac7b313
Gary Hernandez <127606253+GaryH19@users.noreply.github.com>
2025-10-24 19:50:09 -0300

    Delete formulario directory

--- formulario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registro/', views.registro, name='registro'),
-    path('login/', views.login_view, name='login'),
-    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('colaborador_dashboard/', views.colaborador_dashboard, name='colaborador_dashboard'),
-]
