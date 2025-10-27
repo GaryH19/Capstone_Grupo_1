2c3b406718ecfaaf93f3cb9c16e19c7423dfd531
Gary Hernandez <127606253+GaryH19@users.noreply.github.com>
2025-10-27 00:08:09 -0300

    Delete formulario directory

--- formulario/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import TipoUsuario, Proyecto, Fase, ItemDocumento, DocumentoGuia # <-- Añadir DocumentoGuia
-
-admin.site.register(TipoUsuario) 
-admin.site.register(Proyecto)
-admin.site.register(Fase)
-admin.site.register(ItemDocumento)
-admin.site.register(DocumentoGuia) # <-- Añadir esta línea
